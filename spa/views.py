c8742cf7a1a8a261be22bf8910dc70af411694cb
Paudanie <paula.saavedra14@inacapmail.cl>
2024-11-26 13:02:34 -0300

    .

--- spa/views.py
+++ spa/views.py
@@ -18,8 +18,47 @@ PÁGINAS QUE SE VERÁN:
 def inicio(request):
     return render(request, 'paginas/inicio.html')
 
+# Servicios #
+
 def servicios(request):
     return render(request, 'paginas/servicios.html')
+
+def c_servicios(request):
+    return render(request, 'paginas/c_servicios.html')
+
+def u_servicios(request):
+    return render(request, 'paginas/u_servicios.html')
+
+def d_servicios(request):
+    return render(request, 'paginas/d_servicios.html')
+
+# Resrvas #
+
+def reservas(request):
+    return render(request, 'paginas/reservas.html')
+
+def c_reserva(request):
+    return render(request, 'paginas/c_reserva.html')
+
+def u_reserva(request):
+    return render(request, 'paginas/u_reserva.html')
+
+def d_reserva(request):
+    return render(request, 'paginas/d_reserva.html')
+
+# Promos #
+
+def promos(request):
+    return render(request, 'paginas/promos.html')
+
+def c_promos(request):
+    return render(request, 'paginas/c_promos.html')
+
+def u_promos(request):
+    return render(request, 'paginas/u_promos.html')
+
+def d_promos(request):
+    return render(request, 'paginas/d_promos.html')
 
 '''
 def libros(request):
